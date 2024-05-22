00c03bad53de1886d488dce635218ba9dcf1d294
reis071 <reiscorreia071@gmail.com>
2024-05-22 12:00:25 -0300

    feat:add function login required on the views app escolas

--- escolas/views.py
+++ escolas/views.py
@@ -3,23 +3,27 @@ from .form import FormAluno,FormCurso
 from .models import Aluno,Curso
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def index(requests):
     return render(requests,'escolas/index.html')
 
+@login_required
 def alunosCadastrados(requests):
     alunos = Aluno.objects.order_by('id')
     contexto = {'alunos':alunos}
     return render(requests,'escolas/alunosCadastrados.html',contexto)
 
+@login_required
 def curso(request, idAluno):
     aluno = get_object_or_404(Aluno, id=idAluno)
     cursos = Curso.objects.filter(aluno=aluno)
     contexto = {'aluno': aluno, 'cursos': cursos}
     return render(request, 'escolas/curso.html', contexto)
 
+@login_required
 def cadastrarAluno(requests):
     if requests.method != 'POST':
         form = FormAluno()
@@ -33,6 +37,7 @@ def cadastrarAluno(requests):
     contexto = {'form':form}
     return render(requests,'escolas/cadastrarAluno.html',contexto)
 
+@login_required
 def cadastrarCurso(requests, idAluno):
     aluno = Aluno.objects.get(id=idAluno)
     
@@ -51,6 +56,7 @@ def cadastrarCurso(requests, idAluno):
     
     return render(requests,'escolas/cadastrarCurso.html',contexto)
 
+@login_required
 def editarCurso(requests,idCurso):
     cursos = Curso.objects.get(id=idCurso)
     aluno = cursos.aluno
